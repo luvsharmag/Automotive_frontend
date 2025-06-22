e82e076caf5a4e9927dad0812f71bb94f0e4d5bc
luvsharmag <luvsharmagju31@gmail.com>
2025-06-22 07:44:10 +0530

    env import updated

--- src/services/vehicleService.js
+++ src/services/vehicleService.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:3000/api/vehicles";
+// const API_URL = "http://localhost:3000/api/vehicles";
+const API_URL = `${import.meta.env.VITE_API_URL}/vehicles`;
 
 export const getVehicles = async (params) => {
   try {
